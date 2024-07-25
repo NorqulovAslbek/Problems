dde0aa2248ccc845e2184a31f5a6f1adfd1de112
Aslbek Norqulov <aslbeknorqulov246@gmail.com>
2024-07-25 19:43:56 +0500

    2- misol

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ int main() {
     // notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
     // notAlone([3, 4], 3) → [3, 4]
     // notAlone([1, 1, 1, 2], 1) → [1, 1, 1, 2]
-
     int n;
     cout << "n=";
     cin >> n;
@@ -56,5 +55,36 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cout << arr[i] << " ";
     }
+
+
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 2 -masala @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    // Int tipida massiv va n soni berilgan. Massivni birinchi n ta elementlar gruppasi
+    // oxirgi n ta elementlar grupasiga teng bolsa true qaytaring. Bunda n ni oraligi 0
+    // dan massivni uznuligigacha.
+    // sameEnds([5, 6, 45, 99, 13, 5, 6], 0) → true
+    // sameEnds([5, 6, 45, 99, 13, 5, 6], 1) → false
+    // sameEnds([5, 6, 45, 99, 13, 5, 6], 2) → true
+    // sameEnds([5, 6, 45, 99, 13, 5, 6], 3) → false
+
+    int length;
+    cout << "length:";
+    cin >> length;
+    int arr2[length];
+    for (int i = 0; i < length; ++i) {
+        cout << "arr[" << i << "]=";
+        cin >> arr2[i];
+    }
+    int n2;
+    cout << "bu gruppalash kerak bo'lgan arr elementlari soni n2=";
+    cin >> n2;
+    bool result = true;
+    for (int i = 0; i < n2; ++i) {
+        if (arr2[i] != arr2[length - (n2 - i)]) {
+            result = false;
+        }
+    }
+    cout << "Natija:" << boolalpha << result << endl;
+
+
     return 0;
 }
